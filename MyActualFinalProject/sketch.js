af8655e597d58f67327f94046c59a4cc7d577a4e
Khadeejatu <Kd2720@nyu.edu>
2025-05-12 01:58:48 -0400

    All scenes ready!

--- MyActualFinalProject/sketch.js
+++ MyActualFinalProject/sketch.js
@@ -1,198 +1,88 @@
-  //global variables
- let ellipserad= 50;
- let mycircle;
- let mycircle2;
- let hasard0;
- let velocity;
- let pos;
- let rad;
 
-  // function preload ()
-  // {
+let s6image1;
+let s6image2;
+let s6image3;
+let s6image4;
 
-  // }
+ function preload() 
+ {
+  s6image1 = loadImage('media/Story 3 details/details1.png'); 
+  s6image2 = loadImage('media/Story 3 details/details2.png');  
+  s6image3 = loadImage('media/Story 3 details/details3.png'); 
+  s6image4 = loadImage('media/Story 3 details/details4.png'); 
+}
 
   //-------------------------------------------------------------------------------
   function setup() 
   {
     createCanvas(windowWidth, windowHeight);
-    background(255,0,0);
-    mycircle= new circlesf(0, height/2, 255, 255, 255, 10);
-    mycircle2= new circlesb(width, height/2, 0, 0, 0, -10);
+    background(10, 10, 10);
+    imageMode(CENTER);
+  
 
-
+   
 
   }
   //-----------------------------------------------------------------------------------
 
   function draw() 
-  {
-    background(255,0,0);
-  
+{
+  background(10, 10, 10);
 
-  mycircle.move();
-  mycircle2.move();
-  var X= abs((mycircle.x - mycircle2.x))* abs((mycircle.x - mycircle2.x));
-  var Y= abs((mycircle.y - mycircle2.y))*abs((mycircle.y - mycircle2.y));
-  rad= Math.sqrt(X+Y);
-  
-  
-  if (rad<=2*ellipserad)
-  {
-    mycircle2.disappear();
-    mycircle.increase();
+  // Display all images
+  image(s6image1, 160, 171, 200, 200);
+  image(s6image2, 1232, 185, 200, 200);
+  image(s6image4, 375, 650, 300, 270);
+  image(s6image3, 914, 667, 300, 270);
+
+  // Set text styles
+  textFont('Pacifico');
+  textSize(20);
+  textStyle(BOLD); 
+  textAlign(CENTER);
+  fill(255);
+
+  // --- Image 1 ---
+  if (mouseX > 160 - 150 && mouseX < 160 + 150 &&
+      mouseY > 171 - 150 && mouseY < 171 + 150) {
+        image(s6image1, 160, 171, 300, 300);
+    text("1. Aicha's audience also saw very little.", 356, 350);
   }
 
+  // --- Image 2 ---
+  if (mouseX > 1232 - 150 && mouseX < 1232 + 150 &&
+      mouseY > 185 - 150 && mouseY < 185 + 150) {
+        image(s6image2, 1232, 185, 300, 300);
+    text("2. They saw anxiety and confusion.", 1102, 359);
+    text("Most could only see fear and incompetence.", 1102, 384);
   }
 
+  // --- Image 3 ---
+  if (mouseX > 375 - 200 && mouseX < 375 + 200 &&
+      mouseY > 650 - 185 && mouseY < 650 + 185) {
+        image(s6image4, 375, 650, 400, 370);
+    text("3. But Modou heard the whispers of his hair.", 290, 764);
+  }
 
+  // --- Image 4 ---
+  if (mouseX > 914 - 200 && mouseX < 914 + 200 &&
+      mouseY > 667 - 185 && mouseY < 667 + 185) {
+        image(s6image3, 914, 667, 400, 370);
+    text("4. As he saw beyond the surface.", 1300, 627);
+    text("He saw courage and the desire to grow.", 1300, 652);
+  }
+}
     
   //------------------------------------------------------------------------------------
   function mousePressed()
   
   { 
-    
-
     print("mouseX is: ");
     print(mouseX);
     print(", mouseY is: ");
     print(mouseY);
-
+  
   }
   //--------------------------------------------------------------------
-  //My separate classes
-class circlesf
-{ 
-
-  constructor(x, y, r, b, g, veloc) 
-  {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.b = b;
-    this.g = g;
-    this.veloc = veloc;
-    
-    let hasard0 = random(this.veloc); // local variable
-    this.velocity = createVector(hasard0, 0); // class property
-    this.pos = createVector(this.x, this.y);  // class property
-
-  }
-
- display() // this method will just display the little circles
-  {
-    noStroke();
-    fill(this.r,this.g,this.b);
-    ellipse(this.x, this.y, ellipserad,ellipserad);
-  }
-
-  move()
-  {
-
-    noStroke();
-    fill(this.r,this.g,this.b);
-    
-    ellipse(this.pos.x, this.pos.y, ellipserad,ellipserad);
-    this.pos.add(this.velocity);
-    if (this.pos.x >= width+50)
-    {
-      this.pos.x=-10;
-    }
-  }
-
-increase ()
-{
-  noStroke();
-  fill(200); // gray colour to portray merging of colors
-  ellipse(this.x, this.y, 2*ellipserad,2*ellipserad);
-  this.pos.add(this.velocity);
-  if (this.pos.x >= width+50 || this.pos.x <= -50)
-  {
-    this.pos.x=-10;
-  }
-
-}
-
- disappear()
- {
-  noStroke();
-  fill(255, 0,0 );// the same color as the background
-  ellipse(this.x, this.y, ellipserad,ellipserad);
- }
-
-}
-//-------------------------------------------------------------------------------------------------
-
-class circlesb
-{
-  constructor(x, y, r, b, g, veloc) 
-  {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.b = b;
-    this.g = g;
-    this.veloc = veloc;
-    
-    let hasard0 = random(this.veloc); // local variable
-    this.velocity = createVector(hasard0, 0); // class property
-    this.pos = createVector(this.x, this.y);  // class property
-
-  }
-
- display() // this method will just display the little circles
-  {
-    noStroke();
-    fill(this.r,this.g,this.b);
-    ellipse(this.x, this.y, ellipserad,ellipserad);
-  }
-
-  move()
-  {
-
-    noStroke();
-    fill(this.r,this.g,this.b);
-    
-    ellipse(this.pos.x, this.pos.y, ellipserad,ellipserad);
-    this.pos.add(this.velocity);
-    if (this.pos.x <= -50)
-      {
-        this.pos.x=width+50;
-      }
-
-  }
-
-// increase ()
-// {
-//   noStroke();
-//   fill(200); // gray colour to portray merging of colors
-//   ellipse(this.x, this.y, 2*ellipserad,2*ellipserad);
-//   this.pos.add(this.velocity);
-//   if (this.pos.x >= width+50 || this.pos.x <= -50)
-//   {
-//     this.pos.x=-10;
-//   }
-
-// }
-
- disappear()
- {
-  noStroke();
-  fill(255, 0,0 );// the same color as the background
-  ellipse(this.x, this.y, ellipserad,ellipserad);
- }
   
-}
 
-
-
-
-// class Circles
-// {
-
-  
-// }
-
-
-
-  //commentin out and checking
